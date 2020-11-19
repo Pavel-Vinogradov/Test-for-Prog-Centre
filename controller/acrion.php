3be0457d63a3c502940d2b23f7a342d50c4e4098
Pavel <65782910+Pavel-Vinogradov@users.noreply.github.com>
2020-11-19 23:31:26 +0300

    edit index.html ,index.php

--- controller/acrion.php
+++ controller/acrion.php
@@ -1,1 +1,10 @@
 <?php
+require_once "model/db.php";
+$db=new dbConfig();
+
+if (isset($_POST['action'] ) && $_POST['action']=='view')
+{
+    $output='';
+    $data=$db->read();
+    print_r($data);
+}
