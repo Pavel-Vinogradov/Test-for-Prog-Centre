0195b1eb77e1f00b6a1e98f53766e2e13769dafd
Pavel <65782910+Pavel-Vinogradov@users.noreply.github.com>
2020-11-19 21:54:20 +0300

    add test function in db.php

--- model/db.php
+++ model/db.php
@@ -16,12 +16,59 @@ class dbConfig
 //        create standard methods  error handling
         try {
 //           create connection
-            $this->conn=new PDO( "mysql:host=$this->host;dbname=$this->db",$this->name,$this->pass);
-
-        }
-        catch (PDOException $e)
-        {
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db", $this->name, $this->pass);
+            echo 'Соединение установленно';
+        } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
+
+    //creat function insert in SQL DB
+    public function insert($name, $description, $price)
+    {
+
+        $sql = "INSERT INTO catalog.products(name, description, price) VALUE ($name,$description,$price)";
+        $stmt = $this->conn->prepare($sql);// preparing a request
+        $stmt->execute(['name' => $name, 'description' => $description, 'price' => $price]);//array transfer
+        return true;
+    }
+
+// creat function read SQL
+    public function read()
+    {
+        $data = [];
+        $sql = "SELECT * FROM catalog.products";
+        $stmt = $this->conn->prepare($sql);// preparing a request
+        $stmt->execute();//array transfer
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);// create associative array
+        foreach ($result as $item) {
+            $data[] = $item;
+        }
+        return $data;
+    }
+
+
+
+//   creat function update
+    public function update($id, $name, $description, $price)
+    {
+        $sql = "UPDATE catalog.products SET name=$name , description=$description, price=$price WHERE id=$id ";
+        $stmt = $this->conn->prepare($sql);// preparing a request
+        $stmt->execute(['id' => $id, 'name' => $name, 'description' => $description, 'price' => $price]);//array transfer
+        return true;
+    }
+
+// creat function Delete
+    public function delete($id)
+    {
+        $sql = "DELETE FROM catalog.products WHERE id=$id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute(['id' => $id]);
+        return true;
+    }
+
+
 }
+
+$db = new dbConfig();
+
