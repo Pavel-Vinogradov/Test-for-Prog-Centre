5886fb19e7bbec7adb82f50f4abfdcd9b96343c4
Pavel <65782910+Pavel-Vinogradov@users.noreply.github.com>
2020-11-23 01:14:57 +0300

    Updated Db for Heroku MySQL DB

--- model/db.php
+++ model/db.php
@@ -4,10 +4,16 @@
 class dbConfig
 {
     // Class properties: host, name, password, db name,connection
-    private string $host = 'localhost';
-    private string $name = 'root';
-    private string $pass = 'qwerty1234';
-    private string $db = 'catalog';
+//    private string $host = 'localhost';
+//    private string $name = 'root';
+//    private string $pass = 'qwerty1234';
+//    private string $db = 'catalog';
+//    protected ?PDO $conn = null;
+//    public string $timestamp;
+    private string $host = 'b8rg15mwxwynuk9q.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+    private string $name = 'yu952l62ub6r03gl';
+    private string $pass = 'tua4394ez0do22mu';
+    private string $db = 'ez2qcqlipbwyb7mt';
     protected ?PDO $conn = null;
     public string $timestamp;
 
@@ -31,7 +37,7 @@ class dbConfig
 
         $created = $this->timestamp = date('Y-m-d H:i:s');
 
-        $sql = "INSERT INTO catalog.products SET name=:name, price=:price, description=:description,
+        $sql = "INSERT INTO ez2qcqlipbwyb7mt.products SET name=:name, price=:price, description=:description,
                  created=:created";
         $stmt = $this->conn->prepare($sql);// preparing a request
         // bind values
@@ -51,7 +57,7 @@ class dbConfig
     public function read()
     {
         $data = [];
-        $sql = "SELECT * FROM catalog.products";
+        $sql = "SELECT * FROM ez2qcqlipbwyb7mt.products";
         $stmt = $this->conn->prepare($sql);// preparing a request
         $stmt->execute();//array transfer
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);// create associative array
@@ -65,7 +71,7 @@ class dbConfig
 //   creat function update
     public function update($id, $name, $description, $price)
     {
-        $sql = "UPDATE catalog.products SET name=:name , price=:price, description=:description WHERE id=:id ";
+        $sql = "UPDATE ez2qcqlipbwyb7mt.products SET name=:name , price=:price, description=:description WHERE id=:id ";
         $stmt = $this->conn->prepare($sql);// preparing a request
         // bind parameters
         $stmt->bindParam(':name', $name);
@@ -86,7 +92,7 @@ class dbConfig
 // creat function Delete
     public function delete($id)
     {
-        $sql = "DELETE FROM catalog.products WHERE id=$id";
+        $sql = "DELETE FROM ez2qcqlipbwyb7mt.products WHERE id=$id";
         $stmt = $this->conn->prepare($sql);
         $stmt->execute(['id' => $id]);
         return true;
@@ -95,7 +101,7 @@ class dbConfig
     // used for paging products
     public function rowCount()
     {
-        $sql = "SELECT * FROM catalog.products";
+        $sql = "SELECT * FROM ez2qcqlipbwyb7mt.products";
         $stmt = $this->conn->prepare($sql);
         $stmt->execute();
         return $rowCount = $stmt->rowCount();
@@ -104,7 +110,7 @@ class dbConfig
 //get ID
     public function getProductsID($id)
     {
-        $sql = "SELECT *FROM catalog.products WHERE id=$id";
+        $sql = "SELECT *FROM ez2qcqlipbwyb7mt.products WHERE id=$id";
         $stmt = $this->conn->prepare($sql);// preparing a request
         $stmt->execute(['id' => $id]);//array transfer
         return $result = $stmt->fetch(PDO::FETCH_ASSOC);
